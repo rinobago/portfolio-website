73421d6975ffbbe55d23f3380ae39f90a5f6cf20
Rino Bago <rino.bago@gmail.com>
2025-10-27 03:17:07 +0100

    Fixed menu arrow not hidden

--- components/AdditionalServices.tsx
+++ components/AdditionalServices.tsx
@@ -12,21 +12,21 @@ const AdditionalServices = () => {
                             <p className="hidden md:block text-base text-accent w-[488px] h-auto">
                                 Keep your site fast, secure, and up-to-date with monthly performance checks, content updates, and continuous technical support.
                             </p>
-                            <img src="/MenuArrowDown.svg" alt="Menu Arrow Down" className="w-[11.6px] h-[6.1px]" />
+                            <img src="/MenuArrowDown.svg" alt="Menu Arrow Down" className="w-[11.6px] h-[6.1px] inline md:hidden" />
                         </li>
                         <li className="h-auto w-full flex justify-between items-center border-b-1 border-muted px-[5px] py-[15px] md:px-[20px] md:py-[20px]">
                             <h3 className="w-[436px] text-left font-bold text-[clamp(1rem,3vw,1.875rem)]">SEO Optimization</h3>
                             <p className="hidden md:block text-base text-accent w-[488px] h-auto">
                                 Monthly boost your visibility and ranking on Google with targeted keyword setup, metadata tuning, and site speed optimization.
                             </p>
-                            <img src="/MenuArrowDown.svg" alt="Menu Arrow Down" className="w-[11.6px] h-[6.1px]" />
+                            <img src="/MenuArrowDown.svg" alt="Menu Arrow Down" className="w-[11.6px] h-[6.1px] inline md:hidden" />
                         </li>
                         <li className="h-auto w-full flex justify-between items-center border-b-1 border-muted px-[5px] py-[15px] md:px-[20px] md:py-[20px]">
                             <h3 className="w-[436px] text-left font-bold text-[clamp(1rem,3vw,1.875rem)]">AI Chatbot Integration</h3>
                             <p className="hidden md:block text-base text-accent w-[488px] h-auto">
                                 Add a custom AI-powered chatbot that greets visitors, answers questions, or captures leads - perfect for businesses, clinics, or service providers.
                             </p>
-                            <img src="/MenuArrowDown.svg" alt="Menu Arrow Down" className="w-[11.6px] h-[6.1px]" />
+                            <img src="/MenuArrowDown.svg" alt="Menu Arrow Down" className="w-[11.6px] h-[6.1px] inline md:hidden" />
                         </li>
                         <li className="h-auto w-full flex justify-between items-center border-b-1 border-muted px-[5px] py-[15px] md:px-[20px] md:py-[20px]">
                             <h3 className="w-[436px] text-left font-bold text-[clamp(1rem,3vw,1.875rem)]">Analytics & Tracking</h3>
@@ -34,14 +34,14 @@ const AdditionalServices = () => {
                                 I'll set up Google Analytics, visitor tracking, and performance dashboards so you know exactly how your audience interacts with your site - and where you're converting
                                 best.
                             </p>
-                            <img src="/MenuArrowDown.svg" alt="Menu Arrow Down" className="w-[11.6px] h-[6.1px]" />
+                            <img src="/MenuArrowDown.svg" alt="Menu Arrow Down" className="w-[11.6px] h-[6.1px] inline md:hidden" />
                         </li>
                         <li className="h-auto w-full flex justify-between items-center px-[5px] py-[15px] md:px-[20px] md:py-[20px]">
                             <h3 className="w-[436px] text-left font-bold text-[clamp(1rem,3vw,1.875rem)]">Motion & Interaction Design</h3>
                             <p className="hidden md:block text-base text-accent w-[488px] h-auto">
                                 Smooth scroll effects, fade-ins, and subtle animations that make your website feel modern and alive - designed to enhance user engagement and perceived quality.
                             </p>
-                            <img src="/MenuArrowDown.svg" alt="Menu Arrow Down" className="w-[11.6px] h-[6.1px]" />
+                            <img src="/MenuArrowDown.svg" alt="Menu Arrow Down" className="w-[11.6px] h-[6.1px] inline md:hidden" />
                         </li>
                     </ul>
                 </div>
