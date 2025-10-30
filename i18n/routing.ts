e0af9e6022571b612e338a12ed12f659159d52a7
Rino Bago <rino.bago@gmail.com>
2025-10-30 20:19:23 +0100

    Language default changed to hr

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -3,6 +3,6 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
     locales: ["en", "hr"],
 
-    defaultLocale: "en",
+    defaultLocale: "hr",
     localePrefix: "never",
 });
