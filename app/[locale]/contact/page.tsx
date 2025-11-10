2c27ce0461160aa66d3d981f554d351f76d7c11b
Rino Bago <rino.bago@gmail.com>
2025-11-10 19:42:17 +0100

    Added metadata to contact page

--- app/[locale]/contact/page.tsx
+++ app/[locale]/contact/page.tsx
@@ -1,6 +1,12 @@
 import Footer from "@/components/Footer";
 import Form from "@/components/Form/Form";
 import Navbar from "@/components/Navbar/Navbar";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Contact",
+    description: "Contact Rino Bago for inquiries, support, or feedback. I'm here to help and look forward to hearing from you.",
+};
 
 const page = () => {
     return (
